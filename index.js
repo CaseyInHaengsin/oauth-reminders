a75b9713d47008920ce840a96fd26556001ec1c1
caseyinhaengsin <rowley.james74@gmail.com>
2022-04-20 19:37:52 -0600

    Doing cool stuff.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express()
 const cookieParser = require('cookie-parser')
 const crypto = require('crypto')
 const { fetch } = require('undici')
-// const handlebars = require('handlebars')
-// const PORT = process.env.PORT || 3000
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 const { PORT = 3000, COOKIE_SECRET } = process.env
 
@@ -25,7 +25,13 @@ app.get('/', (req, res) => {
 app.get('/login/:provider', (req, res) => {
   const state = crypto.randomUUID()
   res.locals.provider = req.params.provider
-  const redirectUrl = new URL('https://github.com/login/oauth/authorize')
+  if (!process.env[`${req.params.provider.toUpperCase()}_REDIRECT_URL`]) {
+    console.log('in redirect')
+    return res.render('notfound')
+  }
+  const redirectUrl = new URL(
+    process.env[`${req.params.provider.toUpperCase()}_REDIRECT_URL`]
+  )
   redirectUrl.searchParams.set(
     'client_id',
     process.env[`${req.params.provider.toUpperCase()}_CLIENT_ID`]
@@ -51,7 +57,7 @@ app.get('/login/:provider/callback', (req, res) => {
   const { code, state } = req.query
   const signedCookie = req.signedCookies[`${req.params.provider}-provider`]
   if (state !== signedCookie) {
-    return res.status(401).redirect('/')
+    return res.status(401).redirect('/notfound')
   }
 
   const access_token_url = new URL(
@@ -71,6 +77,8 @@ app.get('/login/:provider/callback', (req, res) => {
     'redirect_uri',
     `http://localhost:${PORT}/login/${req.params.provider}/callback`
   )
+  // TODO - make async await and clean up promise chain
+  // TODO - Move prisma logic to folder and _try_ to make function generic for provider
 
   fetch(access_token_url.toString(), {
     method: 'POST',
@@ -81,15 +89,64 @@ app.get('/login/:provider/callback', (req, res) => {
       const providerApiUrl =
         process.env[`${req.params.provider.toUpperCase()}_API_URL`]
       fetch(providerApiUrl, {
-        headers: { authorization: `bearer ${json.access_token}` }
+        headers: { authorization: `Token ${json.access_token}` }
       })
         .then(res => res.json())
         .then(user => {
+          return prisma.githubUser.upsert({
+            where: { id: user.node_id },
+            include: {
+              user: true
+            },
+            update: {
+              login: user.login,
+              avatar_url: user.avatar_url
+            },
+            create: {
+              id: user.node_id,
+              login: user.login,
+              avatar_url: user.avatar_url,
+              user: {
+                create: {
+                  id: crypto.randomUUID(),
+                  username: user.login
+                }
+              }
+            }
+          })
           res.json(user)
+        })
+        .then(async githubUser => {
+          console.log('githubuser', githubUser)
+          const now = new Date()
+          const expiresAt = new Date(now)
+          expiresAt.setDate(expiresAt.getDate() + 14)
+          return prisma.session.create({
+            data: {
+              id: crypto.randomUUID(),
+              userId: githubUser.userId,
+              createdAt: now,
+              expiresAt
+            }
+          })
+        })
+        .then(session => {
+          console.log(session)
+          res.cookie('SID', session.id, {
+            httpOnly: true,
+            expires: session.expiresAt,
+            sameSite: 'strict',
+            signed: true
+          })
+          res.redirect('/')
         })
       // return res.json(json)
     })
   // res.json(req.query)
+})
+
+app.get('/notfound', (req, res) => {
+  res.render('notfound')
 })
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
